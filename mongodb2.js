ef3abbb6510c1d370d44f959ba39a35e2907d0cf
lauravel9 <148814865+lauravel9@users.noreply.github.com>
2024-03-20 15:12:05 -0500

    actividad2

---
+++ mongodb2.js
@@ -0,0 +1,620 @@
+
+db.createCollection('users')
+
+db.users.insertMany([
+    
+{
+    name: 'laura',
+    lastname: 'ruiz',
+    email: 'lauraruiz@gmail.com',
+    city: 'florencia',
+    country: 'italia',
+    salary: 4000,
+    age: 21,
+    height: 160,
+    weight: 60
+},
+
+{
+    name: 'kevin',
+    lastname: 'molina',
+    email: 'kevinmolina@gmail.com',
+    city: 'lyon',
+    country: 'francia',
+    salary: 4000,
+    age: 19,
+    height: 155,
+    weight: 65
+},
+
+{
+    name: 'mateo',
+    lastname: 'velez',
+    email: 'mateovelez@gmail.com',
+    city: 'new york',
+    country: 'estados unidos',
+    salary: 1200,
+    age: 24,
+    height: 180,
+    weight: 90
+},
+
+{
+    name: 'samantha',
+    lastname: 'rojas',
+    email: 'ejemplo@correo.com',
+    city: 'londres',
+    country: 'inglaterra',
+    salary: 3000,
+    age: 26,
+    height: 185,
+    weight: 70
+},
+
+{
+    name: 'mariana',
+    lastname: 'carmona',
+    email: 'marianacarmona@gmail.com',
+    city: 'rio de janeiro',
+    country: 'brasil',
+    salary: 6000,
+    age: 23,
+    height: 175,
+    weight: 65
+},
+
+{
+    name: 'pepe',
+    lastname: 'castro',
+    email: 'otro@ejemplo.com',
+    city: 'berlin',
+    country: 'alemania',
+    salary: 2000,
+    age: 22,
+    height: 170,
+    weight: 75
+},
+
+{
+    name: 'daniel',
+    lastname: 'perez',
+    email: 'danielperez@gmail.com',
+    city: 'tokyo',
+    country: 'japon',
+    salary: 2000,
+    age: 19,
+    height: 175,
+    weight: 65
+},
+
+{
+    name: 'santiago',
+    lastname: 'bermudez',
+    email: 'santiagobermudez@gmail.com',
+    city: 'guadalajara',
+    country: 'mexico',
+    salary: 800,
+    age: 23,
+    height: 175,
+    weight: 55
+},
+
+{
+    name: 'maria',
+    lastname: 'ortiz',
+    email: 'mariaortiz@gmail.com',
+    city: 'marseille',
+    country: 'francia',
+    salary: 1200,
+    age: 29,
+    height: 153,
+    weight: 70
+},
+
+{
+    name: 'juan',
+    lastname: 'perez',
+    email: 'juanperez@gmail.com',
+    city: 'munich',
+    country: 'alemania',
+    salary: 4000,
+    age: 37,
+    height: 185,
+    weight: 105
+},
+
+{
+    name: 'valeria',
+    lastname: 'reyes',
+    email: 'valeriareyes@gmail.com',
+    city: 'ottawa',
+    country: 'canada',
+    salary: 1500,
+    age: 46,
+    height: 185,
+    weight: 120
+},
+
+{
+    name: 'juan pablo',
+    lastname: 'torres',
+    email: 'juanpablotorres@gmail.com',
+    city: 'londres',
+    country: 'inglaterra',
+    salary: 3000,
+    age: 35,
+    height: 180,
+    weight: 70
+},
+
+{
+    name: 'carolina',
+    lastname: 'trujillo',
+    city: 'toronto',
+    country: 'canada',
+    salary: 2000,
+    age: 25,
+    height: 185,
+    weight: 70
+},
+
+{
+    name: 'jhoan',
+    lastname: 'sanchez',
+    email: 'jhoansanchez@gmail.com',
+    city: 'sydney',
+    country: 'australia',
+    salary: 2000,
+    age: 20,
+    height: 175,
+    weight: 150
+},
+
+{
+    name: 'sebastian',
+    lastname: 'valencia',
+    email: 'sebastianvalencia@gmail.com',
+    city: 'miami',
+    country: 'estados unidos',
+    salary: 4000,
+    age: 26,
+    height: 185,
+    weight: 70
+},
+
+{
+    name: 'felipe',
+    lastname: 'ramirez',
+    email: 'feliperamirez@gmail.com',
+    city: 'bogota',
+    country: 'colombia',
+    salary: 1000,
+    age: 27,
+    height: 180,
+    weight: 75
+},
+
+{
+    name: 'pedro',
+    lastname: 'sanchez',
+    email: 'pedrosanchez@gmail.com',
+    city: 'bogota',
+    country: 'colombia',
+    salary: 4000,
+    age: 30,
+    height: 175,
+    weight: 70
+},
+
+{
+    name: 'andres',
+    lastname: 'valdez',
+    email: 'andresvaldez@gmail.com',
+    city: 'toronto',
+    country: 'canada',
+    salary: 4500,
+    age: 25,
+    height: 165,
+    weight: 70
+},
+
+{
+    name: 'matias',
+    lastname: 'fernandez',
+    email: 'matiasfernandez@gmail.com',
+    city: 'bogota',
+    country: 'colombia',
+    salary: 1000,
+    age: 22,
+    height: 165,
+    weight: 150
+},
+
+{
+    name: 'matias',
+    lastname: 'fernandez',
+    email: 'matiasfernandez@gmail.com',
+    city: 'sao paulo',
+    country: 'brasil',
+    salary: 5500,
+    age: 22,
+    height: 165,
+    weight: 130
+},
+
+{
+    name: 'samuel',
+    lastname: 'rios',
+    email: 'samuelrios@gmail.com',
+    city: 'Mumbai',
+    country: 'india',
+    salary: 2000,
+    age: 50,
+    height: 170,
+    weight: 200
+},
+
+{
+    name: 'nicolas',
+    lastname: 'bedoya',
+    email: 'nicolasbedoya@gmail.com',
+    city: 'milan',
+    country: 'italia',
+    salary: 2000,
+    age: 52,
+    height: 170,
+    weight: 150
+},
+
+{
+    name: 'jhon',
+    lastname: 'doe',
+    email: 'jhondoe@gmail.com',
+    city: 'paris',
+    country: 'francia',
+    salary: 1000,
+    age: 26,
+    height: 180,
+    weight: 120
+},
+
+{
+    name: 'camila',
+    lastname: 'bermudez',
+    email: 'jhondoe@gmail.com',
+    city: 'medellin',
+    country: 'colombia',
+    salary: 1500,
+    age: 20,
+    height: 160,
+    weight: 130
+},
+
+{
+    name: 'tobias',
+    lastname: 'agudelo',
+    email: 'borrar@riwi.com',
+    city: 'rio de janeiro',
+    country: 'brasil',
+    salary: 2000,
+    age: 24,
+    height: 175,
+    weight: 130
+},
+
+{
+    name: 'catalina',
+    lastname: 'zuluaga',
+    city: 'munich',
+    country: 'alemania',
+    salary: 3000,
+    age: 27,
+    height: 165,
+    weight: 140
+},
+
+{
+    name: 'diego',
+    lastname: 'botero',
+    email: 'diegobotero@riwi.com',
+    city: 'ottawa',
+    country: 'canada',
+    salary: 0,
+    age: 19,
+    height: 185,
+    weight: 150
+},
+
+{
+    name: 'mikel',
+    lastname: 'gonzales',
+    email: 'mikelgonzales@riwi.com',
+    city: 'sao paulo',
+    country: 'brasil',
+    salary: 2000,
+    age: 29,
+    height: 185,
+    weight: 150
+},
+
+{
+    name: 'manuel',
+    lastname: 'torres',
+    email: 'manuelspam@riwi.com',
+    city: 'bucaramanga',
+    country: 'colombia',
+    salary: 2000,
+    age: 29,
+    height: 185,
+    weight: 150
+},
+
+{
+    name: 'valentina',
+    lastname: 'delgado',
+    email: 'valentinadelgado@riwi.com',
+    city: 'sevilla',
+    country: 'españa',
+    salary: 4000,
+    age: 79,
+    height: 185,
+    weight: 150
+},
+
+{
+    name: 'samantha',
+    lastname: 'fernandez',
+    email: 'samanthafernandez@riwi.com',
+    city: 'bello',
+    country: 'colombia',
+    salary: 1000,
+    age: 25,
+    height: 175,
+    weight: 140
+},
+
+{
+    name: 'juan',
+    lastname: 'alvarez',
+    email: 'valentinadelgado@riwi.com',
+    city: 'sevilla',
+    country: 'españa',
+    salary: 3000,
+    age: 29,
+    height: 195,
+    weight: 160
+}
+
+]);
+
+
+//_______________________________actualizar_______________________________
+
+//-------1----------
+db.users.updateMany(
+    {}, 
+    { $mul: { salary: 1.1 } }
+);
+
+//--------2---------
+db.users.updateMany(
+    {city:"new york"}, 
+    {$set: {city: "los angeles"}}
+);
+
+//--------3---------
+db.users.updateOne(
+    {name:"juan", lastname: "perez"},
+    {$set: {email: "nuevo@correo.com"}}
+);
+
+//--------4---------
+db.users.updateOne(
+    {email: "ejemplo@correo.com"},
+    {$set: {age: 35}}
+);
+
+//--------5---------
+db.users.updateMany(
+    {country: "mexico"},
+    {$set: {country: "canada"}}
+);
+
+//--------6---------
+db.users.updateMany(
+    {},
+    {$inc: {height: 5}}
+);
+
+//--------7---------
+db.users.updateOne(
+    {email: "otro@ejemplo.com"},
+    {$set: {lastname: "González"}}
+);
+
+//--------8---------
+db.users.updateOne(
+    {name: "maria"},
+    {$set: {weight: 140}}
+);
+
+//--------9---------
+db.users.updateMany(
+    {country: "estados unidos"},
+    {$mul: { salary: 1.15 }}
+);
+
+//--------10---------
+db.users.updateOne(
+    {name: "pedro"},
+    {$set: {email: "nuevo_correo@riwi.co"}}
+);
+
+//--------11---------
+db.users.updateMany(
+    {age: {$lt: 30 }},
+    {$set: {age: 30 }} 
+);
+
+//--------12---------
+db.users.updateMany(
+    {salary: {$lt: 3000}},
+    {$mul: {salary: 1.2}}
+);
+
+//--------13---------
+db.users.updateMany(
+    {city: "bogota"},
+    {$set: {city: "medellin"}}
+);
+
+//--------14---------
+db.users.updateMany(
+    {salary: {$gt: 5000}},
+    {$set: {country: "Australia"}}
+);
+
+//--------15---------
+db.users.updateMany(
+    {age: {$gt: 50 }}, 
+    {$inc: {age: -5 }}
+);
+
+//--------16---------
+db.users.updateMany(
+    {weight: {$gt: 200}},
+    {$set: {weight: 180}}
+);
+
+//--------17---------
+db.users.updateMany(
+    {city: "londres"},
+    {$mul: {salary: 1.25}}
+);
+
+//--------18---------
+db.users.updateMany(
+    {salary: {$gt: 4000}},
+    {$set: {lastname: "Smith"}}
+);
+
+//--------19---------
+db.users.updateMany(
+    {name: {$regex: /^A/}}, 
+    {$set: {email: "nuevo@riwi.es"}} 
+);
+
+//--------20---------
+db.users.updateMany(
+    {height: {$lt: 160}},
+    {$set: {city: "París"}}
+);
+
+
+
+
+
+//_______________________________eliminar_______________________________
+db.users.deleteMany({});
+
+//--------1---------
+db.users.deleteMany(
+    {salary: {$lt: 2000}}
+);
+
+//--------2---------
+db.users.deleteMany(
+    {age: {$lt: 25}} 
+);
+
+//--------3---------
+db.users.deleteMany(
+    {city: "paris"}
+);
+
+//--------4---------
+db.users.deleteMany(
+    {weight: {$gt: 180}}
+);
+
+//--------5---------
+db.users.deleteMany(
+    {height: {$lt: 160}}
+);
+
+//--------6---------
+db.users.deleteMany(
+    {name: "jhon", lastname: "doe"}
+);
+
+//--------7---------
+db.users.deleteMany(
+    {email: "borrar@riwi.com"}
+);
+
+//--------8---------
+db.users.deleteMany(
+    {email: {$exists: false}}
+);
+
+//--------9---------
+db.users.deleteMany(
+    {city: "tokyo"}
+);
+
+//--------10---------
+db.users.deleteMany(
+    {age: {$lt: 18}}
+);
+
+//--------11---------
+db.users.deleteMany(
+    {salary: 0}
+);
+
+//--------12---------
+db.users.deleteMany(
+    {city: "new york", salary: {$gt: 5000}}
+);
+
+//--------13---------
+db.users.deleteMany(
+    {email: {$regex: /^spam/}}
+);
+
+//--------14---------
+db.users.deleteMany(
+    {city: "bello", age: {$gt: 50}}
+);
+
+//--------15---------
+db.users.deleteMany(
+    {lastname: "gonzales"}
+);
+
+//--------16---------
+db.users.deleteMany(
+    {age: {$gt: 70}}
+);
+
+//--------17---------
+db.users.deleteMany(
+    {country: "canada", salary: {$lt: 4000}}
+);
+
+//--------18---------
+db.users.deleteMany(
+    {salary: {$gt: 1000}, salary: {$lt: 2000}}
+);
+
+//--------19---------
+db.users.deleteMany(
+    {age: 30}
+);
+
+//--------20---------
+db.users.deleteMany(
+    {height: {$gt: 190}}
+);
+
+
